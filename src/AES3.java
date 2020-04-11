65eeb61c6ae0bfa40444fb1fbb54b38190e68a69
safamSaol <yosi_dekel@walla.com>
2020-04-11 14:02:39 +0300

    memu ido

--- src/AES3.java
+++ src/AES3.java
@@ -133,7 +133,19 @@ public class AES3 {
 
         return bytesArray;
     }
+/*
+C:/Users/user/Desktop/aes/src/key_long
+C:/Users/user/Desktop/aes/src/cipher_long
+C:/Users/user/Desktop/aes/src/plain_long.txt
 
+ */
+    /**
+     *
+     * @param keysPath
+     * @param messagePath
+     * @param outputFile
+     * @throws IOException
+     */
     public void decrypt(String keysPath, String messagePath, String outputFile) throws IOException {
 
         Path path = Paths.get(messagePath);
@@ -157,6 +169,8 @@ public class AES3 {
         catch (Exception e){
             System.out.println("error occured.");
         }
+        path = Paths.get(messagePath);
+        byte[] temppp = Files.readAllBytes(path);
     }
 
 
